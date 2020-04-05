9f3e429b2bab73b735cba55d9aed828748ca33ad
Ashutosh Ranjan <urs.ashu4ever@gmail.com>
2020-04-05 12:10:25 +0530

    initial commit

---
+++ src/main/java/hello/HelloWorld.java
@@ -0,0 +1,17 @@
+package hello;
+
+/**
+ * Created by sssau on 05-04-2020.
+ */
+public class HelloWorld {
+
+    public static void main(String args[]){
+        System.out.println("Hello World");
+    }
+
+    public String hello(String str){
+        return str;
+    }
+}
+
+
---
+++ src/test/java/hello/HelloWorldTest.java
@@ -0,0 +1,24 @@
+package hello;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+/**
+ * Created by sssau on 05-04-2020.
+ */
+public class HelloWorldTest {
+
+    private HelloWorld hw;
+
+    @Before
+    public void SetUp(){
+        hw= new HelloWorld();
+    }
+
+    @Test
+    public void helloTest(){
+    Assert.assertEquals("Hello World", hw.hello("Hello World"));
+    }
+}
